4e0c6e8373c9b555e3f52ae00e2c4a7251b8af93
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-03-18 09:41:57 +0700

    Configure project.urls to handle staticfiles/media files

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-] + urlpatterns
+] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
