931400115af08db01192d214e2b6f6e18cc8d278
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-03-18 10:33:46 +0700

    Add RegistrationForm to users.forms
    We want the registration form to include username, first_name, and last_name so we create custom form containing those fields.

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,29 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    """
+    Form that will be used to create the user in CustomUserAdmin
+    """
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('email', 'username',)
 
 class CustomUserChangeForm(UserChangeForm):
+    """
+    Form that will be used to change the user in CustomUserAdmin
+    """
 
     class Meta:
         model = CustomUser
         fields = ('email', 'username',)
+
+
+class RegistrationForm(UserCreationForm):
+    """
+    Form that will be used in signup process
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'first_name', 'last_name')
