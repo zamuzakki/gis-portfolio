e2d55a4c5e8d01723152dfb5593fa3fdc9beef93
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-03-18 18:13:05 +0700

    Edit portfolio.urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,5 +6,5 @@ from .views import HomePageView, ProfileView, ProfileEditView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('profile', login_required(ProfileView.as_view()), name='profile'),
-    path('user/edit/', login_required(ProfileEditView.as_view()), name='profile_edit'),
+    path('profile/edit', login_required(ProfileEditView.as_view()), name='profile_edit'),
 ]
