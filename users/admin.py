8584fc968fe1489db54cdb9533130f1cddd05e31
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-03-18 10:41:25 +0700

    Add LogEntry to users.admin using LogEntryAdmin Class

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, LogEntry
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username',]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['action', 'email', 'ip',]
+    list_filter = ['action',]
+    search_fields = ['action', 'email', 'ip',]
